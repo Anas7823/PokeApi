a4bc2c0149359e14c07826110120bcb43480dfa5
Anas El Khiat <anas.elkhiat@agecco.io>
2022-10-20 17:42:37 +0200

    img test

--- src/component/Poke.js
+++ src/component/Poke.js
@@ -7,7 +7,7 @@ function Poke() {
 
         <h1>'Nom du pokemon'</h1>
         <h5>Type:</h5>
-        <img src='' alt='_' className='image' />
+        <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Pokebola-pokeball-png-0.png/800px-Pokebola-pokeball-png-0.png' alt='_' className='image' />
 
     </div>)
 
