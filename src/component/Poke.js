2b65ff5f95713367f09b87eca5eb4aeda120cf8b
Anas El Khiat <anas.elkhiat@agecco.io>
2022-10-25 10:22:56 +0200

    visuelle (table)

--- src/component/Poke.js
+++ src/component/Poke.js
@@ -6,11 +6,39 @@ function Poke() {
   return(
     <div className='Poke'>
 
-        <h1>'Nom du pokemon'</h1>
-        <h3 className='type'>Type:</h3>
-        <div className='conteneurImg'>
-          <img src={pokemon} alt='_' className='image' />
-        </div>
+    <table>
+      <tr>
+        <td>     
+          <div className='conteneur'>
+            <h1>'Nom du pokemon'</h1>
+            <h3 className='type'>Type:</h3>
+            <div className='conteneurImg'>
+              <img src={pokemon} alt='_' className='image' />
+            </div>
+          </div>
+        </td>
+
+        <td>     
+          <div className='conteneur'>
+            <h1>'Nom du pokemon'</h1>
+            <h3 className='type'>Type:</h3>
+            <div className='conteneurImg'>
+              <img src={pokemon} alt='_' className='image' />
+            </div>
+          </div>
+        </td>
+
+        <td>     
+          <div className='conteneur'>
+            <h1>'Nom du pokemon'</h1>
+            <h3 className='type'>Type:</h3>
+            <div className='conteneurImg'>
+              <img src={pokemon} alt='_' className='image' />
+            </div>
+          </div>
+        </td>
+      </tr>
+    </table>
 
     </div>)
 
